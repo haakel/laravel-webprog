cc688cd123b4ab4fc440d893789efb58c7ce1b67
haakel <haakel213@gmail.com>
2025-08-21 22:56:59 +0330

    آشنایی با query builder

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('posts')->insert([
+            'title' => 'Test Title',
+            'content' => 'This is a test content for the post.',
+            'is_published' => true,
+        ]);
+    }
+}
